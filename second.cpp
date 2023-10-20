8e37fb4b9c4958e99c79ccc087f3623117338457
jaycom95 <148551105+jaycom95@users.noreply.github.com>
2023-10-20 21:12:39 +0900

    231020
    c언어 강의

---
+++ second.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("%d + %d = %d\n" , 12345, 98765, 12345+98765);
+	
+	printf("%s\n" , "megastudy");
+
+	printf("%.3f\n", 3.141);
+
+	printf("%c%c%c%c\n" , 'T' , 'G' , 'I' , 'F');
+	//현재시간 21:00
+	return 0;
+}
